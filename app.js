6fb1f311e4d893bff428e8e00796e203f56131f6
fxhernandez87 <fxhernandez87@gmail.com>
2019-02-27 21:18:30 -0300

    adding logs to test heroku deployment

--- app.js
+++ app.js
@@ -74,8 +74,9 @@ const init = async () => {
       useNewUrlParser: true,
       useCreateIndex: true
     });
-    console.log(`App listening on port ${env.API_PORT || 3000}`);
-    app.listen(env.API_PORT || 3000);
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`App listening on port ${process.env.PORT || 3000}`);
+    });
   });
 };
 
